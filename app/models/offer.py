a414b0e40d605f09d271cfac87a74abe652dc5f8
tomer-km <101794431+tomer-km@users.noreply.github.com>
2025-03-19 19:48:45 +0200

    Update offer.py

--- app/models/offer.py
+++ app/models/offer.py
@@ -1,1 +1,18 @@
+from typing import List
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app import db
+
+
+class Offer(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    offer_amount: Mapped[int]
+    interest_rate: Mapped[int]
+    offerTerms: Mapped[str] 
+    requestId: Mapped[int]
+    status: Mapped[str]
+
+    user: Mapped["User"] = relationship("User", back_populates="Offer")
